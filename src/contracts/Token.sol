a1de456d7134890ba2e10ad32832776270e34fbe
Constance Kang <linweikang1203@gmail.com>
2019-06-21 14:22:24 -0400

    add additional tests; however, there are TypeError: token.symbol is not a function

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,5 +1,12 @@
 pragma solidity ^0.5.0;
 
-contract Token{
-  string public name = "Coinstance";
+contract Token {
+    string public name = "Coinstance";
+    string public symbol = "CNC";
+    uint256 public decimals = 5;
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 10000 * (10 ** decimals);
+    }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -5,14 +5,35 @@ require('chai')
   .should();
 
 contract('Token', accounts => {
+  const name = 'Coinstance';
+  const symbol = 'CNC';
+  const decimals = '9';
+  const totalSupply = '1000000000';
+  let token;
+
+  beforeEach(async () => {
+    token = await Token.new();
+  });
+
   describe('deployment', () => {
     it('tracks the token name', async () => {
-      //fetch token from blockchian
-      const token = await Token.new();
-      //read token name
-      const name = await token.name();
-      name.should.equal('Coinstance');
-      //check if the token name is coinstance
+      const result = await token.name();
+      result.should.equal(name);
+    });
+
+    it('tracks the token symbol', async () => {
+      const result = await token.symbol();
+      result.should.equal(symbol);
+    });
+
+    it('tracks the token decimals', async () => {
+      const result = await token.decimals();
+      result.toString().should.equal(decimals);
+    });
+
+    it('tracks the total supply', async () => {
+      const result = await token.totalSupply();
+      result.toString().should.equal(totalSupply);
     });
   });
 });
