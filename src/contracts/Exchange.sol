8dfa27c754c3b5e7cdda72f9075e4fbf882f9855
Constance Kang <linweikang1203@gmail.com>
2019-06-21 17:18:09 -0400

    add Exchange contract

---
+++ src/contracts/Exchange.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0
+//will handle of behavior of all decentralized exchange: deposit, withdraw, make order or cancel, trade (fill orders, and charge fee)
+
+contract Exchange {
+
+}
+//set the fee account
+//deposit ether
+//withdraw ether
+//deposit tokens
+//withdraw tokens
+//check balances
+//make order
+//cancel order
+//fill order
+
