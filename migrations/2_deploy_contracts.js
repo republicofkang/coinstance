b360aea2e3f747201b8f15f94e0cce6c365e5e62
Constance Kang <linweikang1203@gmail.com>
2019-06-23 18:05:32 -0400

    seed the exchange

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,11 @@ const Exchange = artifacts.require('Exchange');
 
 module.exports = async function(deployer) {
   const accounts = await web3.eth.getAccounts();
+
+  await deployer.deploy(Token);
+
   const feeAccount = accounts[0];
   const feePercent = 10;
-  await deployer.deploy(Token);
+
   await deployer.deploy(Exchange, feeAccount, feePercent);
 };
