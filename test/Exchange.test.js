4bae2735dcc591cc934a3e4078d195dfa9ca4e65
Constance Kang <linweikang1203@gmail.com>
2019-06-23 17:31:27 -0400

    make withdraw tokens and check balances

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,229 +1,189 @@
-import { tokens, ether, EVM_REVERT, ETHER_ADDRESS } from './helpers';
+import { tokens, EVM_REVERT } from './helpers';
 
 const Token = artifacts.require('./Token');
-const Exchange = artifacts.require('./Exchange');
 
 require('chai')
   .use(require('chai-as-promised'))
   .should();
 
-contract('Exchange', ([deployer, feeAccount, user1]) => {
+contract('Token', ([deployer, receiver, exchange]) => {
+  const name = 'Coinstance';
+  const symbol = 'CNC';
+  const decimals = '18';
+  const totalSupply = tokens(1000000).toString();
   let token;
-  let exchange;
-  const feePercent = 10;
 
   beforeEach(async () => {
-    // Deploy token
     token = await Token.new();
-
-    // Transfer some tokens to user1
-    token.transfer(user1, tokens(100), { from: deployer });
-
-    // Deploy exchange
-    exchange = await Exchange.new(feeAccount, feePercent);
   });
 
   describe('deployment', () => {
-    it('tracks the fee account', async () => {
-      const result = await exchange.feeAccount();
-      result.should.equal(feeAccount);
+    it('tracks the name', async () => {
+      const result = await token.name();
+      result.should.equal(name);
     });
 
-    it('tracks the fee percent', async () => {
-      const result = await exchange.feePercent();
-      result.toString().should.equal(feePercent.toString());
+    it('tracks the symbol', async () => {
+      const result = await token.symbol();
+      result.should.equal(symbol);
+    });
+
+    it('tracks the decimals', async () => {
+      const result = await token.decimals();
+      result.toString().should.equal(decimals);
+    });
+
+    it('tracks the total supply', async () => {
+      const result = await token.totalSupply();
+      result.toString().should.equal(totalSupply);
+    });
+
+    it('assigns the total supply to the deployer', async () => {
+      const result = await token.balanceOf(deployer);
+      result.toString().should.equal(totalSupply);
     });
   });
 
-  describe('fallback', () => {
-    it('reverts when Ether is sent', async () => {
-      await exchange
-        .sendTransaction({ value: 1, from: user1 })
-        .should.be.rejectedWith(EVM_REVERT);
-    });
-  });
-
-  describe('depositing Ether', async () => {
+  describe('sending tokens', () => {
     let result;
     let amount;
-
-    beforeEach(async () => {
-      amount = ether(1);
-      result = await exchange.depositEther({ from: user1, value: amount });
-    });
-
-    it('tracks the Ether deposit', async () => {
-      const balance = await exchange.tokens(ETHER_ADDRESS, user1);
-      balance.toString().should.equal(amount.toString());
-    });
-
-    it('emits a Deposit event', async () => {
-      const log = result.logs[0];
-      log.event.should.eq('Deposit');
-      const event = log.args;
-      event.token.should.equal(ETHER_ADDRESS, 'token address is correct');
-      event.user.should.equal(user1, 'user address is correct');
-      event.amount
-        .toString()
-        .should.equal(amount.toString(), 'amount is correct');
-      event.balance
-        .toString()
-        .should.equal(amount.toString(), 'balance is correct');
-    });
-  });
-
-  describe('withdrawing Ether', async () => {
-    let result;
-    let amount;
-
-    beforeEach(async () => {
-      // Deposit Ether first
-      amount = ether(1);
-      await exchange.depositEther({ from: user1, value: amount });
-    });
 
     describe('success', async () => {
       beforeEach(async () => {
-        // Withdraw Ether
-        result = await exchange.withdrawEther(amount, { from: user1 });
+        amount = tokens(100);
+        result = await token.transfer(receiver, amount, { from: deployer });
       });
 
-      it('withdraws Ether funds', async () => {
-        const balance = await exchange.tokens(ETHER_ADDRESS, user1);
-        balance.toString().should.equal('0');
+      it('transfers token balances', async () => {
+        let balanceOf;
+        balanceOf = await token.balanceOf(deployer);
+        balanceOf.toString().should.equal(tokens(999900).toString());
+        balanceOf = await token.balanceOf(receiver);
+        balanceOf.toString().should.equal(tokens(100).toString());
       });
 
-      it('emits a "Withdraw" event', async () => {
+      it('emits a Transfer event', async () => {
         const log = result.logs[0];
-        log.event.should.eq('Withdraw');
+        log.event.should.eq('Transfer');
         const event = log.args;
-        event.token.should.equal(ETHER_ADDRESS);
-        event.user.should.equal(user1);
-        event.amount.toString().should.equal(amount.toString());
-        event.balance.toString().should.equal('0');
+        event.from.toString().should.equal(deployer, 'from is correct');
+        event.to.should.equal(receiver, 'to is correct');
+        event.value
+          .toString()
+          .should.equal(amount.toString(), 'value is correct');
       });
     });
 
     describe('failure', async () => {
-      it('rejects withdraws for insufficient balances', async () => {
-        await exchange
-          .withdrawEther(ether(100), { from: user1 })
+      it('rejects insufficient balances', async () => {
+        let invalidAmount;
+        invalidAmount = tokens(100000000); // 100 million - greater than total supply
+        await token
+          .transfer(receiver, invalidAmount, { from: deployer })
           .should.be.rejectedWith(EVM_REVERT);
+
+        // Attempt transfer tokens, when you have none
+        invalidAmount = tokens(10); // recipient has no tokens
+        await token
+          .transfer(deployer, invalidAmount, { from: receiver })
+          .should.be.rejectedWith(EVM_REVERT);
+      });
+
+      it('rejects invalid recipients', async () => {
+        await token.transfer(0x0, amount, { from: deployer }).should.be
+          .rejected;
       });
     });
   });
 
-  describe('depositing tokens', () => {
+  describe('approving tokens', () => {
     let result;
     let amount;
 
+    beforeEach(async () => {
+      amount = tokens(100);
+      result = await token.approve(exchange, amount, { from: deployer });
+    });
+
     describe('success', () => {
-      beforeEach(async () => {
-        amount = tokens(10);
-        await token.approve(exchange.address, amount, { from: user1 });
-        result = await exchange.depositToken(token.address, amount, {
-          from: user1
-        });
+      it('allocates an allowance for delegated token spending on exchange', async () => {
+        const allowance = await token.allowance(deployer, exchange);
+        allowance.toString().should.equal(amount.toString());
       });
 
-      it('tracks the token deposit', async () => {
-        // Check exchange token balance
-        let balance;
-        balance = await token.balanceOf(exchange.address);
-        balance.toString().should.equal(amount.toString());
-        // Check tokens on exchange
-        balance = await exchange.tokens(token.address, user1);
-        balance.toString().should.equal(amount.toString());
-      });
-
-      it('emits a Deposit event', async () => {
+      it('emits an Approval event', async () => {
         const log = result.logs[0];
-        log.event.should.eq('Deposit');
+        log.event.should.eq('Approval');
         const event = log.args;
-        event.token.should.equal(token.address, 'token address is correct');
-        event.user.should.equal(user1, 'user address is correct');
-        event.amount
+        event.owner.toString().should.equal(deployer, 'owner is correct');
+        event.spender.should.equal(exchange, 'spender is correct');
+        event.value
           .toString()
-          .should.equal(amount.toString(), 'amount is correct');
-        event.balance
-          .toString()
-          .should.equal(amount.toString(), 'balance is correct');
+          .should.equal(amount.toString(), 'value is correct');
       });
     });
 
     describe('failure', () => {
-      it('rejects Ether deposits', async () => {
-        await exchange
-          .depositToken(ETHER_ADDRESS, tokens(10), { from: user1 })
-          .should.be.rejectedWith(EVM_REVERT);
-      });
-
-      it('fails when no tokens are approved', async () => {
-        // Don't approve any tokens before depositing
-        await exchange
-          .depositToken(token.address, tokens(10), { from: user1 })
-          .should.be.rejectedWith(EVM_REVERT);
+      it('rejects invalid spenders', async () => {
+        await token.approve(0x0, amount, { from: deployer }).should.be.rejected;
       });
     });
   });
 
-  describe('withdrawing tokens', async () => {
+  describe('delegated token transfers', () => {
     let result;
     let amount;
 
+    beforeEach(async () => {
+      amount = tokens(100);
+      await token.approve(exchange, amount, { from: deployer });
+    });
+
     describe('success', async () => {
       beforeEach(async () => {
-        // Deposit tokens first
-        amount = tokens(10);
-        await token.approve(exchange.address, amount, { from: user1 });
-        await exchange.depositToken(token.address, amount, { from: user1 });
-
-        // Withdraw tokens
-        result = await exchange.withdrawToken(token.address, amount, {
-          from: user1
+        result = await token.transferFrom(deployer, receiver, amount, {
+          from: exchange
         });
       });
 
-      it('withdraws token funds', async () => {
-        const balance = await exchange.tokens(token.address, user1);
-        balance.toString().should.equal('0');
+      it('transfers token balances', async () => {
+        let balanceOf;
+        balanceOf = await token.balanceOf(deployer);
+        balanceOf.toString().should.equal(tokens(999900).toString());
+        balanceOf = await token.balanceOf(receiver);
+        balanceOf.toString().should.equal(tokens(100).toString());
       });
 
-      it('emits a "Withdraw" event', async () => {
+      it('resets the allowance', async () => {
+        const allowance = await token.allowance(deployer, exchange);
+        allowance.toString().should.equal('0');
+      });
+
+      it('emits a Transfer event', async () => {
         const log = result.logs[0];
-        log.event.should.eq('Withdraw');
+        log.event.should.eq('Transfer');
         const event = log.args;
-        event.token.should.equal(token.address);
-        event.user.should.equal(user1);
-        event.amount.toString().should.equal(amount.toString());
-        event.balance.toString().should.equal('0');
+        event.from.toString().should.equal(deployer, 'from is correct');
+        event.to.should.equal(receiver, 'to is correct');
+        event.value
+          .toString()
+          .should.equal(amount.toString(), 'value is correct');
       });
     });
 
     describe('failure', async () => {
-      it('rejects Ether withdraws', async () => {
-        await exchange
-          .withdrawToken(ETHER_ADDRESS, tokens(10), { from: user1 })
+      it('rejects insufficient amounts', async () => {
+        // Attempt transfer too many tokens
+        const invalidAmount = tokens(100000000);
+        await token
+          .transferFrom(deployer, receiver, invalidAmount, { from: exchange })
           .should.be.rejectedWith(EVM_REVERT);
       });
 
-      it('fails for insufficient balances', async () => {
-        // Attempt to withdraw tokens without depositing any first
-        await exchange
-          .withdrawToken(token.address, tokens(10), { from: user1 })
-          .should.be.rejectedWith(EVM_REVERT);
+      it('rejects invalid recipients', async () => {
+        await token.transferFrom(deployer, 0x0, amount, { from: exchange })
+          .should.be.rejected;
       });
-    });
-  });
-
-  describe('checking balances', async () => {
-    beforeEach(async () => {
-      exchange.depositEther({ from: user1, value: ether(1) });
-    });
-
-    it('returns user balance', async () => {
-      const result = await exchange.balanceOf(ETHER_ADDRESS, user1);
-      result.toString().should.equal(ether(1).toString());
     });
   });
 });
