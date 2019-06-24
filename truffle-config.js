6f4dc5b2a1247d9e5bb667b8dadfc5cfa5cc35dc
Constance Kang <linweikang1203@gmail.com>
2019-06-23 22:08:38 -0400

    waiting for kovan deploy

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,8 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey')
+const privateKeys = process.env.PRIVATE_KEYS || '';
 
 module.exports = {
   networks: {
@@ -10,6 +12,17 @@ module.exports = {
       network_id: '*' // Match any network id
     }
   },
+  kovan:{
+    provider:function(){
+      return new HDWalletProvider(
+        privateKeys.split(',') //array of account private keys
+        `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}` //url to an ethereum node
+      )
+    },
+    gas: 5000000,
+    gasPrice: 25000000000,
+    network_id:42
+  }
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
   compilers: {
